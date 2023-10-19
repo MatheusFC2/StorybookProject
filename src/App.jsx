a7fcc588243883fe4b0095807afc535db9038a6a
MatheusFC2 <matheusfreitascampos@gmail.com>
2023-10-19 20:38:14 -0300

    feat: add and set setup NextUI2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import {Button} from "@nextui-org/react";
+
 function App() {
 
   return (
     <>
       <h1 className="text-red-500">testando aqui o tailwdincs com bootrap</h1>
+      <Button color="primary">
+        Button
+      </Button>
     </>
   )
 }
